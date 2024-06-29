87ff252028691065ade41d10f62735523cfd66d4
KaiGeffen <geffenkai@gmail.com>
2024-06-29 14:35:57 -0400

    Updated backend to use blurring api

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,37 @@
 import express from 'express';
 
 const app = express();
+const apikey = "fuNfYFNWsXrkC7Oh9MppmxxHNzi8bZsk"
+const api_base_url = "https://api.apilayer.com/face_pixelizer/url?url="
 
 app.get('/', (req, res) => {
-    res.send("Hello world")
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", apikey);
+
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+    };
+
+    // TODO Grab arbitrary files off of your local machine
+    const todo = 'https://kaigeffen.com/assets/bio.jpg'
+    const api_url = `${api_base_url}${todo}`
+
+    fetch(api_url, requestOptions)
+    .then(response => response.text())
+    .then(jsonString => {
+        const resultObject = JSON.parse(jsonString)
+
+        // Get the URL for the now blurred image
+        const resultUrl = resultObject.result
+
+        // Send that as the response
+        res.send(resultUrl)
+    })
+    .catch(error => res.status(400).send({
+        message: error
+    }));
 })
 
 const URL = "http://localhost"
