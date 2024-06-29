e91506d6907d3de12fb2858c3eeeeadf8db1a748
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-29 15:09:10 -0400

    file picker works

--- frontend/src/components/InputForm.tsx
+++ frontend/src/components/InputForm.tsx
@@ -21,6 +21,8 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { ChangeEventHandler, useState } from "react"
+import e from "express"
 
 
 export function ProfileForm() {
@@ -46,6 +48,23 @@ export const InputForm = () => {
     }
 
 
+    const [file, setFile] = useState<File | null>()
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (event.target.files) {
+            const newFile = event.target.files[0]
+            setFile(newFile)
+            console.log(newFile)
+        }
+    }
+
+    const handleSubmit = () => {
+        if (file)
+            console.log("uploading file")
+    }
+
+
+
     return (
         <>
             <div className="flex flex-col border gap-10 p-5 border-gray rounded">
@@ -63,18 +82,9 @@ export const InputForm = () => {
                         </input>
                     </div>
 
-                    <div className="flex items-center justify-center w-full">
-                        <label className="flex flex-col w-64 md:min-w-[500px] items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                </svg>
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                            </div>
-                            <input id="dropzone-file" type="file" className="hidden" />
-                        </label>
-                    </div>
+
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Small file input</label>
+                    <input className="block w-full mb-5 text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="small_size" type="file" onChange={handleChange} />
 
                     <Button className="bg-orange-500 rounded text-white">
                         Submit
